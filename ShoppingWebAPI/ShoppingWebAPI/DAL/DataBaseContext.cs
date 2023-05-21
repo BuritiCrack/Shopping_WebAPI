9b1b691e239aa73d9eedcedd93f5bcec841d48a5
BuritiCrack <jmbur@BURITICRACK>
2023-05-20 19:45:36 -0500

    New Country API created

---
+++ ShoppingWebAPI/ShoppingWebAPI/DAL/DataBaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingWebAPI.DAL.Entities;
+
+namespace ShoppingWebAPI.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; } // Countries es el nombre de la tabla por eso simepre van en plural
+       //public DbSet<Category> Categories { get; set; }
+       //public DbSet<State> States { get; set; }
+       //public DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+           // modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            //modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique(); // Índice Compuesto
+            //modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique(); // Índice Compuesto
+        }
+    }
+}
---
+++ ShoppingWebAPI/ShoppingWebAPI/DAL/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace ShoppingWebAPI.DAL.Entities
+{
+    public class Country : Entity
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] // Not null
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener como maximo {1} caracteres")]
+        [Display(Name = "País")]
+        public string Name { get; set; }
+    }
+}
